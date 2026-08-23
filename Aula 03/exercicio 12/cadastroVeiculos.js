b7ea80cc9745acc5a824851251b8c95dc94ab9b0
elias-ml-dev <eliass.s@outlook.com.br>
2026-08-23 17:57:55 -0300

    feat: adicionado exercicio 12 veiculos aula 3

---
+++ Aula 03/exercicio 12/cadastroVeiculos.js
@@ -0,0 +1,107 @@
+const mysql = require("mysql2");
+const readline = require("readline-sync")
+
+
+// Conexão com o MySQL
+const conexao = mysql.createConnection({
+    host:"127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "sistema_veiculos"    
+});
+
+//Função para cadastrar v
+function cadastrarVeiculo() {
+
+    const modelo = readline.question("Digite o modelo do veiculo: ");
+    const placa = readline.question("Digite a placa do veiculo: ");
+    
+
+    const insert = "INSERT INTO veiculos (modelo, placa) VALUES (?,?)";
+
+    conexao.query(insert,[modelo, placa], function (erro){
+
+        if (erro) {
+            console.log("Erro ao cadastrar.");
+            console.log(erro);
+        } else {
+            console.log("Veículo cadastrado com sucesso!");
+        }
+         menu();
+    });  
+}
+
+// Função para excluir 
+
+function excluirVeiculo() {
+
+    const id = readline.questionInt("Digite o ID do veiculo: ");
+
+    const deletar ="DELETE FROM veiculos WHERE id = ?";
+
+    conexao.query(deletar, [id], function (erro, resultado){
+
+        if (erro) {
+            console.log("Erro ao excluir o veiculo.");
+        } else if (resultado.affectedRows === 0) {
+            console.log("Veículo não encontrado.");
+        } else {
+            console.log("Veículo excluído com sucesso!");
+        }
+
+        menu();
+    });
+}
+
+
+// Função para listar 
+function listarVeiculo() {
+    const sql = "SELECT * FROM veiculos";
+
+    conexao.query(sql, function (erro, veiculos){
+        if (erro) {
+            console.log("Erro ao buscar veículos.");
+        }else if (veiculos.length === 0) {
+            console.log("\nNenhum veículo cadastrado.");
+        }else {
+            console.log("\n--- VEÍCULOS ---");
+            veiculos.forEach(function (veiculo){
+                console.log(
+                    veiculo.id + " - " +
+                    veiculo.modelo + " - " +
+                    veiculo.placa + " - " 
+                );
+            });
+        }
+        menu();
+    });
+}
+
+// Menu principal
+function menu() {
+
+    console.log("\n===== MENU =====");
+    console.log("1 - Cadastrar veículo");
+    console.log("2 - Listar veículos");
+    console.log("3 - Excluir veículo");
+    console.log("0 - Sair");
+
+    const opcao = readline.questionInt("Escolha uma opcao: ");
+
+    if (opcao === 1) {
+        cadastrarVeiculo();
+    } else if (opcao === 2) {
+        listarVeiculo();
+    } else if (opcao === 3){
+        excluirVeiculo();
+    } else if (opcao === 0) {
+        console.log("Programa encerrado");
+        conexao.end();
+    } else {
+        console.log("Opção invalida")
+        menu();
+    }
+}
+
+// Inicia o programa
+menu();
